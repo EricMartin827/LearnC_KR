f416ce223c079d7ec7dc5a6200b3bd9761951c99
eMart <ericmartin827@gmail.com>
2016-06-18 12:18:13 -0400

    ch_one/1-13.c : added

---
+++ ch_one/1-13.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#define BLANK ' '
+#define TAB '\t'
+#define NEWLINE '\n'
+#define MAXLENGTH 1000
+
+#define OUT 0
+#define IN 1
+#define NOT_LETTER(X) (X == BLANK) || (X == TAB) || (X == NEWLINE)
+
+
+int
+main(void)
+{
+        int c;
+        int blen = 0;
+        int len = 0;
+        int data[MAXLENGTH];
+        uint8_t state = OUT;
+
+        memset(data, 0, sizeof(data));
+
+        while ((c = getchar()) != EOF) {
+                if (NOT_LETTER(c)) {
+                        if (state == IN) {
+                                data[len]++;
+                        }
+                        state = OUT;
+                } else if (state == OUT) {
+                        state = IN;
+                }
+                len = (state == IN) ? (len + 1) : 0;
+
+                if (len < MAXLENGTH) {
+                        blen = (len > blen) ? len : blen;
+                } else {
+                        data[len - 1]++;
+                        while ((c = getchar()) != EOF
+                               && !(NOT_LETTER(c)));
+                }
+        }
+
+        if (len > 0)
+                data[len]++;
+        for (c = 0; c <= blen; c++) {
+                int i = 0;
+                printf("\nLength %d: ", c);
+                while (i < data[c]) {
+                        printf("*");
+                        i++;
+                }
+                printf("\n");
+        }
+        return 0;
+}

f973ad2155ffc4b1d67e5f8b99c923e8139a3eef
eMart <ericmartin827@gmail.com>
2016-06-28 18:50:55 -0400

    ch_seven/7-1.c: completed

--- ch_seven/7-1.c
+++ ch_seven/7-1.c
@@ -4,26 +4,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define LOWER "tolower"
-#define UPPER "toupper"
-#define FUNC(A) (A)
-
+#define LOWER "./tolower"
+#define UPPER "./toupper"
+#define LEN 10
 int
 main(int argc, char **argv)
 {
 	int c;
-	char cmd[8];
-	//int (*func)(char c);
+	char cmd[LEN];
 	if (argc != 1) {
 		errno = EPROTO;
 		perror("no arguments.");
 		return EXIT_FAILURE;
 	}
 	strncpy(cmd, argv[0], sizeof(cmd));
-	if ((!cmd[0]) &&
-	    (((c = strcmp(cmd, LOWER)) != 0)) ||
-	    (strcmp(cmd, UPPER) != 0))) {
-		errno = EINVAL;
+	if ((cmd[LEN - 1] != '\0') || 
+            (((c = strcmp(cmd, LOWER)) != 0) && ((strcmp(cmd, UPPER)) != 0))) {
+                puts(cmd);
+                errno = EINVAL;
 		perror("argv[0] = tolower or toupper");
 		return EXIT_FAILURE;
 	}
